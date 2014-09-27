278aeb87b9faca39e7110a6626105b721fd37445
SorokinDmitry <sorokin.dmitr@yandex.ru>
2014-09-27 12:53:36 +0400

    Добавлена проверка ввода первого аргумента

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -20,27 +20,30 @@ public class Main {
         if (args.length == 1) {
             String portString = args[0];
             port = Integer.valueOf(portString);
+        } else {
+            System.out.append("Use port as the first argument");
+            System.exit(1);
         }
 
         System.out.append("Starting at port: ").append(String.valueOf(port)).append('\n');
 
         AccountService accountService = new AccountService();
 
-        Servlet signupservlet = new SignupServlet(accountService);
-        Servlet signinservlet = new SigninServlet(accountService);
-        Servlet mainservlet = new MainPageServlet(accountService);
-        Servlet logoutservlet = new LogoutServlet(accountService);
-        Servlet adminservlet = new AdminPageServlet(accountService);
-        Servlet profileservlet = new ProfileServlet(accountService);
+        Servlet signupServlet = new SignupServlet(accountService);
+        Servlet signinServlet = new SigninServlet(accountService);
+        Servlet mainServlet = new MainPageServlet(accountService);
+        Servlet logoutServlet = new LogoutServlet(accountService);
+        Servlet adminServlet = new AdminPageServlet(accountService);
+        Servlet profileServlet = new ProfileServlet(accountService);
 
         Server server = new Server(port);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(signupservlet), "/auth/signup");
-        context.addServlet(new ServletHolder(signinservlet), "/auth/signin");
-        context.addServlet(new ServletHolder(logoutservlet), "/auth/logout");
-        context.addServlet(new ServletHolder(mainservlet), "/main");
-        context.addServlet(new ServletHolder(profileservlet), "/profile");
-        context.addServlet(new ServletHolder(adminservlet), "/admin");
+        context.addServlet(new ServletHolder(signupServlet), "/auth/signup");
+        context.addServlet(new ServletHolder(signinServlet), "/auth/signin");
+        context.addServlet(new ServletHolder(logoutServlet), "/auth/logout");
+        context.addServlet(new ServletHolder(mainServlet), "/main");
+        context.addServlet(new ServletHolder(profileServlet), "/profile");
+        context.addServlet(new ServletHolder(adminServlet), "/admin");
 
         ResourceHandler resource_handler = new ResourceHandler();
         resource_handler.setDirectoriesListed(true);
