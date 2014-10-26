bdc6f2e2c363d1e91d8c538796d838e74faaf4c4
grechkin_pogrebnyakov <grechkin_pogrebnyakov@gmail.com>
2014-10-26 15:20:53 +0300

    before adding tests

---
+++ src/tests/base/AccountServiceTest.java
@@ -0,0 +1,19 @@
+package base;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testAddUser() throws Exception {
+
+    }
+}
